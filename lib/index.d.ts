8762322690156fc17662b2acb47d06bc43deefb5
Tom Deryckere <Tom.Deryckere@gmail.com>
2019-06-10 10:27:17 -0400

    Fix dev dependencies for node types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -6,5 +6,5 @@
  * @param data
  * @param args
  */
-export declare function generateChunkedCalls(call: (...args: []) => any, chunkSize: number, concurrency: number, data: [], ...args: []): IterableIterator<any>;
-export declare const concu: (call: () => any, chunkSize: number, concurrency: number, data: any[], args: any[]) => Promise<[{}, {}, {}, {}, {}, {}, {}, {}, {}, {}]>;
+export declare function generateChunkedCalls(call: (...args: any[]) => any, chunkSize: number, concurrency: number, data: any[], ...args: any[]): IterableIterator<any>;
+export declare const concu: (call: (...args: any[]) => any, chunkSize: number, concurrency: number, data: any[], args: any[]) => Promise<any[]>;
